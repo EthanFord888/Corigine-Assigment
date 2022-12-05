1cca4f82c8d96cf935f1e1ea1c4623ec6fe4e52e
EthanFord888 <u19025433@tuks.co.za>
2022-12-05 22:14:05 +0200

    Add Digit counter

--- Ethan_Fibonacci.py
+++ Ethan_Fibonacci.py
@@ -11,78 +11,26 @@ or
 type "pip install tabulate" in python terminal
 """
 import numpy as np
-from tabulate import tabulate
-import cmath
-import matplotlib.pyplot as plt
 #==========================================
 #Variables 
 FibonacciArray=0
 
 #==========================================
-round_value=4
-#==========================================
-#Quadratic equation 
-def solve (a,b,c):
-    a = a
-    b = b
-    c = c
-    
-    d = (b**2) - (4*a*c)
-    
-    sol1 = (-b-cmath.sqrt(d))/(2*a)
-    sol2 = (-b+cmath.sqrt(d))/(2*a)
-    
-    print(sol1)
-    print(sol2)
-    
-    if sol1.real<sol2.real:
-        return sol1
-    else:
-        return sol2
-#Scientific notation 
-def sci(x):
-    return_string='=ERROR='
-    
-    if (x<1):
-        temp=x*1000
-        if (temp>=1):
-             return '{} m'.format(round(x*1000,round_value))
-        temp=x*1E6
-        if (temp>=1):
-             return '{} µ'.format(round(x*1E6,round_value))
-        temp=x*1E9
-        if (temp>=1):
-             return '{} n'.format(round(x*1E9,round_value))
-    
-    if (x>1):
-        temp=x
-        if (temp<1000):
-             return '{} '.format(round(x,round_value))
-         
+#Determine number of digits
+def getDigits(x):
+     count = 0
+     while(x>0):
+          count=count+1
+          x=x//10
+     return count
 
-        temp=x/1000
-        if (temp<1000):
-             return '{} k'.format(round(x/1000,round_value))
-        temp=x/1E6
-        if (temp<1000):
-             return '{} M'.format(round(x/1E6,round_value))
-        temp=x/1E9
-        if (temp<1000):
-             return '{} G'.format(round(x/1E9,round_value))
-         
-    
-    return return_string    
-    
 #==========================================
+
+
+
 n=int(input("Enter number:"))
-count=0
-while(n>0):
-    count=count+1
-    n=n//10
-print("The number of digits in the number are:",count)
+print("The number of digits in the number are:",getDigits(n))
 
-#Write data into table for No Ls
-mydata=[('round_value','{} '.format(sci(round_value))),
-            ]
-headers=["Parameter","Value"]
-print(tabulate(mydata, headers=headers))
+
+
+
